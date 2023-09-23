73faac2c6c26c4d16e3a5aab94c1114f42a0be8d
franco66 <david_franco@outlook.com>
2023-09-22 22:18:02 -0300

    gg

--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -13,7 +13,7 @@ const Workouts = () => {
     <section className='section'>
         {/* section title*/}
       <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0' data-aos='fade-up' data-aos-delay='200'>
-      <img src={icon} />
+      <img src={icon} alt='' />
       <h2 className='h2 section-title'>{title} <span className='text-red-500'>.</span></h2>
     </div>
 
